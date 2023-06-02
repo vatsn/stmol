241741d06fa48b5ae288bf97bd62e5845826e5a9
Vatsal <vatsalnautiyal01@gmail.com>
2023-06-03 00:15:45 +0530

    com2

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ import io
 
 st.set_page_config(page_title="Stmol", page_icon="🧬",layout="wide")
 st.sidebar.markdown('''
-    [![GitHub](https://badgen.net/badge/icon/GitHub?icon=github&color=black&label)](https://github.com/napoles-uach/stmol)
+    [![GitHub](https://badgen.net/badge/icon/GitHub?icon=github&color=black&label)](https://github.com/vatsn/stmol/tree/master)
     ''')  
 with io.open(f'README.md', mode='r', encoding='utf-8') as f:           
     st.markdown(f.read(),unsafe_allow_html=True)
--- pages/1_📝_Documentation.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.sidebar.markdown('''
-    [![GitHub](https://badgen.net/badge/icon/GitHub?icon=github&color=black&label)](https://github.com/napoles-uach/stmol/blob/master/functions.md)
-    ''')   
-st.title("Functions")
-with open(f'functions.md', 'r') as f:           
-    st.markdown(f.read())
